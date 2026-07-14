0266039a3eca13c77d66172351caf353f6097492
Soushiyan <soushiyan.sohrabi@gmail.com>
2026-07-14 14:47:06 +0330

    Session 2: Jinja2 templates, static files, base layout

--- app.py
+++ app.py
@@ -1,25 +1,42 @@
-from flask import Flask 
+from flask import Flask, render_template 
   
 app = Flask(__name__) 
   
 @app.route('/') 
 def home(): 
-    return 'Hi! my name is Soushiyan. My first website is working with Flask.'
+    return render_template('index.html')
+
 @app.route('/about') 
 def about(): 
-    return 'This page is about us.' 
+    return render_template('about.html')
   
+
 @app.route('/contact') 
 def contact(): 
     return 'Please contact us at this email address.'
 
+
 @app.route('/me') 
 def me(): 
-    return 'My first name is Soushiyan and my last name is Sohrabi. I am a programmer' 
+    return render_template("index.html", name='Soushiyan', age=15)
+
 
 @app.route('/calc/<int:a>/<int:b>')
 def calc(a, b):
     return str(a + b)
+
+
+@app.route('/subjects')
+def subjects():
+    topics = ['math', 'physic', 'science']
+    return render_template('subjects.html', topics=topics)
+
+
+@app.route("/hobbies")
+def hobbies():
+    hobbies_list = ["Fencing", "Reading", "Video Games"]
+    return render_template("hobbies.html", hobbies=hobbies_list)
+
 
 if __name__ == '__main__': 
     app.run(debug=True) 
